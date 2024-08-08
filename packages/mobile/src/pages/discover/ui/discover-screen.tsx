4c2108ae459619811ad36eead09bdd43ed2938e9
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2024-08-08 16:32:44 +0700

    Configured tailwind

--- packages/mobile/src/pages/discover/ui/discover-screen.tsx
+++ packages/mobile/src/pages/discover/ui/discover-screen.tsx
@@ -1,15 +1,24 @@
 import { View } from 'react-native'
-import { TextInput } from 'react-native-paper'
+import { TextInput, Button } from 'react-native-paper'
+import { ScrollView } from 'shared/ui/rn'
 
 export function DiscoverScreen() {
   return (
-    <View className="flex-1 justify-center p-4">
-      <TextInput
-        multiline
-        mode="outlined"
-        label="Outlined input"
-        placeholder="Type something"
-      />
-    </View>
+    <ScrollView className="flex-1" contentContainerStyle="p-5 pb-10 flex-grow">
+      <View className="flex-1 justify-center items-center">
+        <TextInput
+          multiline
+          mode="outlined"
+          label="Enter prompt"
+          placeholder="Type here a detailed description of what you want to see in your artwork"
+          className="w-full min-h-[200]"
+        />
+      </View>
+      <View className="items-center">
+        <Button icon="magic-staff" mode="contained">
+          Make magic
+        </Button>
+      </View>
+    </ScrollView>
   )
 }
