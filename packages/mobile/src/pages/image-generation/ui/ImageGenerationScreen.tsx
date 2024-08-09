ad395260e6996b2c0cfec0c50f9331e34b52201e
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2024-08-09 21:39:49 +0700

    Complete validation

--- packages/mobile/src/pages/image-generation/ui/ImageGenerationScreen.tsx
+++ packages/mobile/src/pages/image-generation/ui/ImageGenerationScreen.tsx
@@ -1,22 +1,37 @@
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMemo } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { Keyboard, View } from 'react-native'
-import { Text, TextInput } from 'react-native-paper'
+import { HelperText, Text, TextInput } from 'react-native-paper'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { twMerge } from 'tailwind-merge'
 import * as yup from 'yup'
 import { ScrollView, Button } from 'shared/ui/styled'
 import { StylesList } from './StylesList'
 
-export const generationSchema = yup
-  .object({
-    prompt: yup.string().min(3).max(300).required(),
-    style: yup.string().max(100).nullable(),
-  })
-  .required()
-
 export function ImageGenerationScreen() {
   const { t } = useTranslation()
+
+  const generationSchema = useMemo(
+    () =>
+      yup
+        .object({
+          prompt: yup
+            .string()
+            .min(3, ({ min }) =>
+              t('screens.generation.errors.minLength', { min })
+            )
+            .max(300, ({ max }) =>
+              t('screens.generation.errors.maxLength', { max })
+            )
+            .required(t('screens.generation.errors.required')),
+          style: yup.string().max(100).nullable(),
+        })
+        .required(),
+    [t]
+  )
+
   const { control, handleSubmit, setValue } = useForm({
     resolver: yupResolver(generationSchema),
     defaultValues: {
@@ -29,6 +44,7 @@ export function ImageGenerationScreen() {
 
   const handleStartPress = ({ prompt }: { prompt: string }) => {
     Keyboard.dismiss()
+    console.log({ prompt })
   }
 
   return (
@@ -44,38 +60,51 @@ export function ImageGenerationScreen() {
         >
           <Controller
             control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <View className="px-1 py-6 bg-white rounded-2xl border-gray-200 border-[1px] min-h-[210] justify-between mb-8">
-                <View>
-                  <Text variant="titleLarge" className="mx-4">
-                    {t('screens.generation.inputLabel')}
-                  </Text>
-                  <TextInput
-                    underlineColor="transparent"
-                    activeUnderlineColor="transparent"
-                    multiline
-                    placeholder={t('screens.generation.inputPlaceholder')}
-                    className="bg-transparent px-0"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                </View>
-                <View className="items-end px-4">
-                  <Button
-                    compact
-                    mode="outlined"
-                    icon="dice-multiple"
-                    contentStyle="flex-row-reverse px-1"
+            render={({ field: { onChange, onBlur, value }, formState }) => {
+              const hasError = formState.submitCount > 0 && !formState.isValid
+
+              return (
+                <>
+                  <View
+                    className={twMerge(
+                      'px-1 py-6 bg-white rounded-2xl border-gray-200 border-[1px] min-h-[210] justify-between',
+                      hasError && 'border-red-700'
+                    )}
                   >
-                    {t('screens.generation.surpriseButton')}
-                  </Button>
-                </View>
-              </View>
-            )}
+                    <View>
+                      <Text variant="titleLarge" className="mx-4">
+                        {t('screens.generation.inputLabel')}
+                      </Text>
+                      <TextInput
+                        underlineColor="transparent"
+                        activeUnderlineColor="transparent"
+                        multiline
+                        placeholder={t('screens.generation.inputPlaceholder')}
+                        className="bg-transparent px-0"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    </View>
+                    <View className="items-end px-4">
+                      <Button
+                        compact
+                        mode="outlined"
+                        icon="dice-multiple"
+                        contentStyle="flex-row-reverse px-1"
+                      >
+                        {t('screens.generation.surpriseButton')}
+                      </Button>
+                    </View>
+                  </View>
+                  <View className="h-8">
+                    <HelperText type="error" visible={hasError}>
+                      {formState.errors.prompt?.message}
+                    </HelperText>
+                  </View>
+                </>
+              )
+            }}
             name="prompt"
           />
           <StylesList
@@ -84,15 +113,22 @@ export function ImageGenerationScreen() {
         </View>
       </ScrollView>
       <View className="absolute left-0 bottom-0 right-0 items-center pb-5">
-        <Button
-          icon="creation"
-          mode="contained"
-          className="rounded-full"
-          contentStyle="px-4 py-2"
-          onPress={handleSubmit(handleStartPress)}
-        >
-          {t('screens.generation.startButton')}
-        </Button>
+        <Controller
+          control={control}
+          name="prompt"
+          render={({ formState }) => (
+            <Button
+              icon="creation"
+              mode="contained"
+              className="rounded-full"
+              contentStyle="px-4 py-2"
+              onPress={handleSubmit(handleStartPress)}
+              disabled={formState.submitCount > 0 && !formState.isValid}
+            >
+              {t('screens.generation.startButton')}
+            </Button>
+          )}
+        />
       </View>
     </View>
   )
