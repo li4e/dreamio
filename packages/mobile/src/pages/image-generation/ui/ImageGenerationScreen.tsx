bd9eee01cd245e0a22bc7e860c7a5f778f39d523
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2024-08-09 19:29:20 +0700

    Fixed ImageGenerationScreen layout

--- packages/mobile/src/pages/image-generation/ui/ImageGenerationScreen.tsx
+++ packages/mobile/src/pages/image-generation/ui/ImageGenerationScreen.tsx
@@ -31,44 +31,45 @@ export function ImageGenerationScreen() {
         contentContainerStyle="px-5 pb-[95] flex-grow"
         keyboardShouldPersistTaps="handled"
       >
-        <View className="flex-1" style={{ paddingTop: insets.top + 20 }}>
-          <View className="flex-grow">
-            <View className="px-1 py-6 bg-white rounded-2xl border-gray-200 border-[1px] min-h-[210] flex-grow justify-between mb-8">
-              <View>
-                <Text variant="titleLarge" className="mx-4">
-                  {t('screens.generation.inputLabel')}
-                </Text>
-                <TextInput
-                  underlineColor="transparent"
-                  activeUnderlineColor="transparent"
-                  multiline
-                  placeholder={t('screens.generation.inputPlaceholder')}
-                  className="bg-transparent px-0"
-                />
-              </View>
-              <View className="items-end px-4">
-                <Button
-                  compact
-                  mode="outlined"
-                  icon="dice-multiple"
-                  contentStyle="flex-row-reverse px-1"
-                >
-                  {t('screens.generation.randomButton')}
-                </Button>
-              </View>
-            </View>
-            <View className="flex-row justify-between items-center min-h-[35] mb-3">
-              <Text variant="titleMedium" className="mb-1">
-                {t('screens.generation.styleLabel')}
+        <View
+          className="flex-grow justify-end"
+          style={{ paddingTop: insets.top + 20 }}
+        >
+          <View className="px-1 py-6 bg-white rounded-2xl border-gray-200 border-[1px] min-h-[210] justify-between mb-8">
+            <View>
+              <Text variant="titleLarge" className="mx-4">
+                {t('screens.generation.inputLabel')}
               </Text>
-              {style && (
-                <Chip compact onClose={() => setStyle(null)}>
-                  {style}
-                </Chip>
-              )}
+              <TextInput
+                underlineColor="transparent"
+                activeUnderlineColor="transparent"
+                multiline
+                placeholder={t('screens.generation.inputPlaceholder')}
+                className="bg-transparent px-0"
+              />
             </View>
-            <StylesList selectedStyle={style} onSelect={handleStylePress} />
+            <View className="items-end px-4">
+              <Button
+                compact
+                mode="outlined"
+                icon="dice-multiple"
+                contentStyle="flex-row-reverse px-1"
+              >
+                {t('screens.generation.randomButton')}
+              </Button>
+            </View>
           </View>
+          <View className="flex-row justify-between items-center min-h-[35] mb-3">
+            <Text variant="titleMedium" className="mb-1">
+              {t('screens.generation.styleLabel')}
+            </Text>
+            {style && (
+              <Chip compact onClose={() => setStyle(null)}>
+                {style}
+              </Chip>
+            )}
+          </View>
+          <StylesList selectedStyle={style} onSelect={handleStylePress} />
         </View>
       </ScrollView>
       <View className="absolute left-0 bottom-0 right-0 items-center pb-5">
