58859623e859ace8b7db6b27520e8306f96e66a5
Ilsur Gabdulkhakov <support@ilsur.me>
2025-01-15 17:54:08 +0800

    Added supporting of additional languages

--- packages/mobile/src/app/lib/dayjs.ts
+++ packages/mobile/src/app/lib/dayjs.ts
@@ -1,7 +1,13 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import i18next from 'i18next'
+import { i18next } from './i18next'
 import 'dayjs/locale/en'
+import 'dayjs/locale/ru'
+import 'dayjs/locale/uk'
+import 'dayjs/locale/zh'
+import 'dayjs/locale/zh-cn'
+import 'dayjs/locale/zh-tw'
+import 'dayjs/locale/zh-hk'
 
 dayjs.extend(relativeTime)
 
--- packages/mobile/src/shared/locales/index.ts
+++ packages/mobile/src/shared/locales/index.ts
@@ -1,5 +1,13 @@
 import en from './en.json'
+import ru from './ru.json'
+import uk from './uk.json'
+import zhS from './zh-Hans.json'
+import zhT from './zh-Hant.json'
 
 export const locales = {
   en: { translation: en },
+  ru: { translation: ru },
+  uk: { translation: uk },
+  zh: { translation: zhT },
+  'zh-Hans': { translation: zhS },
 } as const
