fd829c971e5b96d35e94f5174435901a8a915d32
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2025-02-08 16:50:19 +0800

    Fix useOnSaveImage and it's translations

--- apps/mobile/src/shared/ui/CachedImage/useOnSaveImage.ts
+++ apps/mobile/src/shared/ui/CachedImage/useOnSaveImage.ts
@@ -2,29 +2,40 @@ import { useCallback } from 'react'
 import { saveImage } from './ImageCache'
 import { useTranslation } from 'react-i18next'
 import { Linking } from 'react-native'
-import { useSnackbar } from 'shared/ui/Snackbar'
+import { SnackBarVariant, useSnackbar } from 'shared/ui/Snackbar'
 
 export function useOnSaveImage() {
   const { showSnackbar } = useSnackbar()
   const { t } = useTranslation()
 
-  return useCallback((url: string) => {
-    const saved = saveImage(url)
+  return useCallback(async (url: string) => {
+    const saved = await saveImage(url)
     if (saved === null) {
       showSnackbar(
         {
-          title: t('components.snackBar.saveImageError.title'),
-          description: t('components.snackBar.saveImageError.description'),
+          title: t('components.snackBar.saveImage.noAccess.title'),
+          description: t('components.snackBar.saveImage.noAccess.description'),
         },
         {
+          variant: SnackBarVariant.ERROR,
           rightAction: {
             handler: () => {
               Linking.openSettings()
             },
-            label: t('components.snackBar.saveImageError.button'),
+            label: t('components.snackBar.saveImage.noAccess.button'),
           },
         }
       )
+    } else if (saved === true) {
+      showSnackbar({
+        title: t('components.snackBar.saveImage.success.title'),
+        description: t('components.snackBar.saveImage.success.description'),
+      })
+    } else {
+      showSnackbar({
+        title: t('components.snackBar.saveImage.error.title'),
+        description: t('components.snackBar.saveImage.error.title'),
+      })
     }
   }, [])
 }
