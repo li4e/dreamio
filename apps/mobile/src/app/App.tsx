2635e6da7ab6987c1ebc598d45c0a38284f07c9d
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2025-02-13 17:52:50 +0800

    Bump v.1.3.2 b.33

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
 import { AppRouter } from './AppRouter'
 import { Providers } from './Providers'
-import * as SystemUI from 'expo-system-ui'
 import * as SplashScreen from 'expo-splash-screen'
 
-SystemUI.setBackgroundColorAsync('transparent')
 SplashScreen.preventAutoHideAsync()
 SplashScreen.setOptions({
   duration: 300,
