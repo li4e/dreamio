b2d36b3104b4a2856b3dd705b8b5cd205950069d
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2025-02-16 16:27:42 +0800

    Fix language code detection

--- apps/mobile/src/app/lib/getMachingLocale.ts
+++ apps/mobile/src/app/lib/getMachingLocale.ts
@@ -1,25 +1,25 @@
-import * as Localization from "expo-localization";
-import { SUPPORTED_LANGUAGES } from "shared/translations";
+import * as Localization from 'expo-localization'
+import { SUPPORTED_LANGUAGES } from 'shared/translations'
 
 export const getMatchingLocale = (): string => {
-  const deviceLocales = Localization.getLocales();
+  const deviceLocales = Localization.getLocales()
 
   for (const locale of deviceLocales) {
-    const tag = locale.languageTag;
-    const code = locale.languageTag.replace(`-${locale.regionCode}`, "");
+    const tag = locale.languageTag
+    const code = locale.languageTag.replace(`-${locale.languageRegionCode}`, '')
 
     if (SUPPORTED_LANGUAGES.includes(tag)) {
-      return tag;
+      return tag
     }
 
     const matchedLanguage = SUPPORTED_LANGUAGES.find((lang) => {
-      return lang.startsWith(code);
-    });
+      return lang.startsWith(code)
+    })
 
     if (matchedLanguage) {
-      return matchedLanguage;
+      return matchedLanguage
     }
   }
 
-  return "en-US";
-};
+  return 'en-US'
+}
