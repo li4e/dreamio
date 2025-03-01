b907ac2c348d403fcd882f4dcc6d9f994392df47
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2025-03-01 15:18:26 +0800

    Restyle GenerationResult screen

--- apps/mobile/src/pages/generation-result/ui/GenerationResultScreen.tsx
+++ apps/mobile/src/pages/generation-result/ui/GenerationResultScreen.tsx
@@ -2,22 +2,15 @@ import { useNavigation } from '@react-navigation/native'
 import * as Clipboard from 'expo-clipboard'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { View, ScrollView } from 'react-native'
-import {
-  Appbar,
-  Chip,
-  Divider,
-  IconButton,
-  Text,
-  useTheme,
-} from 'react-native-paper'
-import { ActivityIndicator } from 'react-native-paper'
+import { View } from 'react-native'
+import { Appbar, Icon, IconButton, Text, useTheme } from 'react-native-paper'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import {
   GenerationEntity,
-  GenerationEntityStatus,
+  GenerationSettings,
   useGenerationDataService,
+  useShowEnhanceInfoDialog,
+  useShowStyleInfoDialog,
 } from 'entities/generation'
 import { useDialog } from 'shared/ui/Dialog'
 import { RelativeTime } from 'shared/ui/RelativeTime'
@@ -27,6 +20,12 @@ import { CachedImage, shareImage, useOnSaveImage } from 'shared/ui/CachedImage'
 import { getAspectRatioFromSize } from 'shared/ui/AspectedRatioView'
 import { AspectedRatioView } from 'shared/ui/AspectedRatioView'
 import { HEADER_HEIGHT } from 'shared/constants'
+import { BlurView } from 'expo-blur'
+import Animated, {
+  useAnimatedRef,
+  useScrollViewOffset,
+} from 'react-native-reanimated'
+import { StickyHeaderWrapper } from 'shared/ui/StickyHeaderWrapper'
 
 const bottomOffset = 70
 
@@ -41,105 +40,82 @@ export function GenerationResultScreen(
   const onDelete = useGenDelete(generation)
   const onCopy = useOnCopy(generation)
   const onRework = useOnRework(generation)
+  const scrollViewRef = useAnimatedRef<Animated.ScrollView>()
+  const scrollY = useScrollViewOffset(scrollViewRef)
+  const showEnahceInfoDialog = useShowEnhanceInfoDialog()
+  const showStyleInfoDialog = useShowStyleInfoDialog()
+  const onSaveImage = useOnSaveImage()
+  const image = generation.images[0]
 
   return (
     <View className="flex-1">
-      <Header onDeletePress={onDelete} generation={generation} />
-      {generation && generation.status === GenerationEntityStatus.SUCCESS ? (
-        <>
-          <ScrollView
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        className="flex-1"
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingBottom: insets.bottom + bottomOffset,
+        }}
+        stickyHeaderIndices={[0]}
+      >
+        <StickyHeaderWrapper scrollY={scrollY}>
+          <Header onDeletePress={onDelete} generation={generation} />
+        </StickyHeaderWrapper>
+        <AspectedRatioView ratio={getAspectRatioFromSize(generation)}>
+          <CachedImage
+            source={generation.images[0]}
             className="flex-1"
-            contentContainerStyle={{
-              flexGrow: 1,
-              paddingBottom: insets.bottom + bottomOffset,
-            }}
-          >
-            <AspectedRatioView ratio={getAspectRatioFromSize(generation)}>
-              <CachedImage
-                source={generation.images[0]}
-                className="flex-1"
-                contentFit="contain"
-                contentPosition="center"
-                style={{ backgroundColor: colors.backdrop }}
-              />
-            </AspectedRatioView>
-            <View className="flex-grow">
-              <View className="flex-row items-center justify-between p-5">
-                <View className="flex-row items-center">
-                  <MaterialCommunityIcons
-                    name="calendar"
-                    size={20}
-                    color={colors.secondary}
-                  />
-                  <Text variant="labelLarge" className="ml-2">
-                    <RelativeTime time={generation.createdAt} />
-                  </Text>
-                </View>
-                {generation.style && (
-                  <Chip mode="outlined" compact icon="palette">
-                    <Text variant="titleSmall">{generation.style}</Text>
-                  </Chip>
-                )}
-              </View>
-              <Divider className="w-full mb-5" />
-              <View className="px-5">
-                <View className="flex-row items-center justify-between mb-1">
-                  <Text variant="titleMedium" className="mb-3">
-                    {t('screens.generationResult.promptLabel')}
-                  </Text>
-                  <View className="flex-row items-center">
-                    <Text variant="labelMedium" className="mr-2">
-                      {t('screens.generationResult.enhancer')}
-                    </Text>
-                    <MaterialCommunityIcons
-                      size={20}
-                      name={
-                        generation.enhance
-                          ? 'checkbox-marked-circle'
-                          : 'checkbox-blank-circle-outline'
-                      }
-                      color={colors.primary}
-                    />
-                  </View>
-                </View>
-                <View
-                  className="py-5 px-5 rounded-lg pr-10 min-h-[100]"
-                  style={{ backgroundColor: colors.inverseOnSurface }}
-                >
-                  <Text>{generation.prompt}</Text>
-                  <IconButton
-                    className="absolute right-0 bottom-0"
-                    icon="content-copy"
-                    size={18}
-                    onPress={onCopy}
-                  />
-                </View>
-              </View>
+            contentFit="contain"
+            contentPosition="center"
+            style={{ backgroundColor: colors.backdrop }}
+          />
+        </AspectedRatioView>
+        <View className="flex-grow">
+          <View className="flex-row items-center justify-between p-5">
+            <View className="flex-row items-center">
+              <Icon source="calendar" size={20} color={colors.secondary} />
+              <Text variant="labelLarge" className="ml-2">
+                <RelativeTime time={generation.createdAt} />
+              </Text>
             </View>
-          </ScrollView>
-          <View
-            className="absolute left-0 bottom-0 right-0 flex-row justify-between px-5"
-            style={{ paddingBottom: insets.bottom + 16 }}
-          >
-            <Button mode="contained-tonal" icon="brush" onPress={onRework}>
-              {t('screens.generationResult.reworkButton')}
-            </Button>
-            <Button
-              mode="contained"
-              icon="share-variant"
-              onPress={() =>
-                shareImage(generation.images[0], generation.prompt)
-              }
-            >
-              {t('screens.generationResult.shareButton')}
-            </Button>
+            <View className="flex-row">
+              <IconButton
+                onPress={() => shareImage(image, generation.prompt)}
+                icon="share-variant"
+              />
+              <IconButton onPress={() => onSaveImage(image)} icon="download" />
+            </View>
           </View>
-        </>
-      ) : (
-        <View className="flex-1 items-center justify-center">
-          <ActivityIndicator animating={true} />
+          <View className="px-5">
+            <GenerationSettings
+              data={generation}
+              className="mb-2"
+              onEnhancePress={showEnahceInfoDialog}
+              onStylePress={showStyleInfoDialog}
+            />
+            <View
+              className="py-5 px-5 rounded-lg pr-10 min-h-[100]"
+              style={{ backgroundColor: colors.inverseOnSurface }}
+            >
+              <Text>{generation.prompt}</Text>
+              <IconButton
+                className="absolute right-0 bottom-0"
+                icon="content-copy"
+                size={18}
+                onPress={onCopy}
+              />
+            </View>
+          </View>
         </View>
-      )}
+      </Animated.ScrollView>
+      <View
+        className="absolute left-0 bottom-0 right-0 flex-row justify-center px-5"
+        style={{ paddingBottom: insets.bottom + 16 }}
+      >
+        <Button mode="contained" icon="brush" onPress={onRework}>
+          {t('screens.generationResult.reworkButton')}
+        </Button>
+      </View>
     </View>
   )
 }
@@ -152,18 +128,18 @@ interface HeaderProps {
 function Header(props: HeaderProps) {
   const { onDeletePress, generation } = props
   const { goBack } = useNavigation()
-  const onSaveImage = useOnSaveImage()
+
+  const { dark } = useTheme()
 
   return (
-    <Appbar.Header className="bg-transparent">
-      <Appbar.BackAction onPress={goBack} />
-      <Appbar.Content title="" />
-      <Appbar.Action
-        icon="download"
-        onPress={() => onSaveImage(generation.images[0])}
-      />
-      <Appbar.Action icon="trash-can-outline" onPress={onDeletePress} />
-    </Appbar.Header>
+    <BlurView intensity={100} tint={dark ? 'dark' : 'light'}>
+      <Appbar.Header className="bg-transparent">
+        <Appbar.BackAction onPress={goBack} />
+        <Appbar.Content title="" />
+
+        <Appbar.Action icon="trash-can-outline" onPress={onDeletePress} />
+      </Appbar.Header>
+    </BlurView>
   )
 }
 
