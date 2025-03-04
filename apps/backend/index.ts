ebabdb8481ac0531885be198808a1b142df653f4
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2025-03-04 16:13:22 +0800

    Add backend app and cofigure deployments process

---
+++ apps/backend/index.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions/v2';
+import { AppModule } from './src/app.module';
+
+const createNestServer = async (): Promise<express.Express> => {
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  app.setGlobalPrefix('api/v1');
+  await app.init();
+  return expressApp;
+};
+
+let server: express.Express | null = null;
+
+export const api = functions.https.onRequest({}, async (request, response) => {
+  if (!server) {
+    server = await createNestServer();
+  }
+  server(request, response);
+});
