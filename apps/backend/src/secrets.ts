b17fc7199d62e569c8f246389096fcc82c898a34
Ilsur Gabdulkhakov <li4e.ru@gmail.com>
2026-04-14 15:47:48 +1200

    Delete backend

--- apps/backend/src/secrets.ts
+++
@@ -1,8 +0,0 @@
-import { defineSecret } from 'firebase-functions/params';
-
-export const secretsMap = {
-  telegramBotToken: defineSecret('TELEGRAM_BOT_TOKEN'),
-  telegramChatID: defineSecret('TELEGRAM_CLAIM_CHAT_ID'),
-};
-
-export default Object.values(secretsMap);
